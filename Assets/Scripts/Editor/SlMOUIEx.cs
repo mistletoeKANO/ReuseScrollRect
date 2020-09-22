3349d90a6a7a1eed94e8808b05d474e1ffc8aa0f
赵前道 <1649881580@qq.com>
2020-09-22 16:13:10 +0800

    init

---
+++ Assets/Scripts/Editor/SlMOUIEx.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace UnityEngine.UI
+{
+    public class SlMOUIEx
+    {
+        [MenuItem("GameObject/SLMOUI/ReuseScrollRect",false,10)]
+        static void AddSlMoReuseScrollRect(MenuCommand menuCommand)
+        {
+            var curSelectTrans = Selection.gameObjects[0].transform;
+            var newObj = new GameObject("ReuseScrollRect",typeof(RectTransform));
+            newObj.transform.SetParent(curSelectTrans);
+            newObj.transform.localPosition = Vector2.zero;
+            newObj.AddComponent<CanvasRenderer>();
+            var color = newObj.AddComponent<Image>();
+            color.color = new Color(0.29f, 0.29f, 0.29f, 0.26f);
+            newObj.AddComponent<ReuseScrollRect>();
+            Selection.activeObject = newObj;
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+
+namespace UnityEngine.UI
+{
+    public class Test : MonoBehaviour
+    {
+        void Start()
+        {
+            ReuseScrollRect scrollGridVertical = gameObject.GetComponent<ReuseScrollRect>();
+            scrollGridVertical.AddCellListener(this.OnCellUpdate);
+            scrollGridVertical.SetCellCount(153);
+        }
+
+        private void OnCellUpdate(ReuseScrollItem cell)
+        {
+            cell.gameObject.GetComponentInChildren<Text>().text = cell.index.ToString();
+        }
+    }
+}
